550677f9053a31960aad352be061d1b52b5fda3f
mariowillaredt <mario-willaredt@web.de>
2025-03-24 19:28:40 +0100

    add axis labels to testbench

--- test.py
+++ test.py
@@ -10,9 +10,11 @@ def voigt_profile(self, x, sigma, gamma):
 if __name__ == "__main__":
     x_values = np.linspace(1392.5 - 5,1392.5 + 5, 1000)
     sigma = .68
-    gamma = .34
+    gamma = .14
     y_values = scipy.special.voigt_profile(x_values - 1392.5, sigma, gamma)
     plt.plot(x_values, y_values)
     plt.grid(alpha=0.5)
+    plt.xlabel("wavlength in nm")
+    plt.ylabel("absorption")
     plt.show()
     print(matplotlib.matplotlib_fname())
