306b213defa2b8e881f8b7255becd5237e591010
mariowillaredt <mario-willaredt@web.de>
2025-03-27 00:26:59 +0100

    further inspection and close up to voigt profile for future curve fitting

--- test.py
+++ test.py
@@ -12,10 +12,15 @@ if __name__ == "__main__":
     absorps = np.genfromtxt("absorptions0.csv", delimiter=",")
     print(np.shape(lambdas))
     print(np.shape(absorps))
-    x_values = np.linspace(1392.5 - 0.1,1392.5 + 0.1, 3870)
+    stellen = np.where(np.isclose(absorps, 1e-6, atol=1e-7))
+    print(stellen)
+    lambdas = lambdas[2553:2792]
+    absorps = absorps[2553:2792]
+    stelle = np.argmax(absorps)
+    x_values = np.linspace(lambdas[0], lambdas[2792 - 2553-1], np.shape(lambdas)[0])
     sigma = .68
     gamma = .14
-    y_values = 10**(-6) * scipy.special.voigt_profile(x_values - 1392.5, sigma, gamma)
+    y_values = 10**(-6) * scipy.special.voigt_profile(x_values - lambdas[stelle], sigma, gamma)
     plt.plot(x_values, y_values)
     plt.plot(lambdas, absorps)
     plt.grid(alpha=0.5)
