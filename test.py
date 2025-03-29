49919a07f2a936c9882952c5bdf5e64a4a0e6451
mariowillaredt <mario-willaredt@web.de>
2025-03-29 15:59:42 +0100

    use scipy curve fit for retrieving parameterized representation of Voigt profile

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 import numpy as np
 import scipy.special
+from scipy.optimize import curve_fit
 import matplotlib
 import matplotlib.pyplot as plt
 
-def voigt_profile(x, sigma, gamma):
-    z = (x + 1j * gamma) / (sigma * np.sqrt(2))
+def voigt_profile_wofz(x, sigma, gamma, x_offset):
+    z = ((x - x_offset) + 1j * gamma) / (sigma * np.sqrt(2))
     return np.real(scipy.special.wofz(z)) / (sigma * np.sqrt(2 * np.pi))
+
+def voigt_profile(x, sigma, gamma, x_offset, magnitude):
+    return magnitude * scipy.special.voigt_profile(x - x_offset, sigma, gamma)
 
 if __name__ == "__main__":
     lambdas = np.genfromtxt("lambdas0.csv", delimiter=",")
@@ -14,17 +18,30 @@ if __name__ == "__main__":
     print(np.shape(absorps))
     stellen = np.where(np.isclose(absorps, 1e-6, atol=1e-7))
     print(stellen)
-    lambdas = lambdas[2553:2792]
-    absorps = absorps[2553:2792]
+    lambdas = lambdas[2453:2892]
+    absorps = absorps[2453:2892]
     stelle = np.argmax(absorps)
+    print(lambdas[stelle])
     x_values = np.linspace(lambdas[0], lambdas[2792 - 2553-1], np.shape(lambdas)[0])
     sigma = .68
     gamma = .14
-    y_values = 10**(-6) * scipy.special.voigt_profile(x_values - lambdas[stelle], sigma, gamma)
-    plt.plot(x_values, y_values)
+    z_values = 10**(-6) * voigt_profile_wofz(x_values, sigma, gamma, lambdas[stelle])
+    y_values = 10**(-6) * voigt_profile(lambdas, sigma, gamma, lambdas[stelle], 1.0)
+    initial_guess = [0.03,0.03,1392.5,1e-6]
+    popt, pcov = curve_fit(voigt_profile, lambdas, absorps, p0=initial_guess)
+    fitted_sigma, fitted_gamma, fitted_x_offset, fitted_magnitude = popt
+    print("Fitted parameters:")
+    print(f"Sigma: {fitted_sigma}")
+    print(f"Gamma: {fitted_gamma}")
+    print(f"X Offset: {fitted_x_offset}")
+    print(f"Magnitude: {fitted_magnitude}")
+    # plt.plot(x_values, z_values)
+    # plt.plot(x_values, y_values)
     plt.plot(lambdas, absorps)
+    plt.plot(lambdas, voigt_profile(lambdas, *popt), label="Fitted Voigt Profile")
+    plt.legend()
     plt.grid(alpha=0.5)
     plt.xlabel("wavlength in nm")
     plt.ylabel("absorption")
+    plt.title("Voigt Profile Fitting")
     plt.show()
-    print(matplotlib.matplotlib_fname())
